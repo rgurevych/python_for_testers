6b00b704c9548872341711801bfdd2b4449ded47
Rostyslav Gurevych <rostikg@gmail.com>
2016-12-10 19:10:09 +0100

    Changed default browser to Chrome

--- test_add_group.py
+++ test_add_group.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.action_chains import ActionChains
 import time, unittest
 
