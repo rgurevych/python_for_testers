73c74934fd0efa8a81c50275168d9eaf547399cd
Rostyslav Gurevych <rostikg@gmail.com>
2016-12-15 19:35:52 +0100

    Common fixture created for both tests

--- application.py
+++ application.py
@@ -54,6 +54,26 @@ class Application:
         wd = self.wd
         wd.find_element_by_link_text("group page").click()
 
+    def add_new_contact(self, contact):
+        wd = self.wd
+        # initiate adding contact
+        wd.find_element_by_link_text("add new").click()
+        # fill in some fields
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        # create contact
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
     def logout(self):
         wd = self.wd
         wd.find_element_by_link_text("Logout").click()
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,63 +1,18 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.chrome.webdriver import WebDriver
-import time, unittest
+import pytest
 from contact import Contact
+from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
-class test_add_contact(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def open_homepage(self, wd):
-        wd.get("http://localhost/addressbook/")
 
-    def login(self, wd):
-        self.open_homepage(wd)
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+def test_test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.add_new_contact(Contact("Mister", "Musterman", "+490123456789", "tester@test.com"))
+    app.logout()
 
-    def add_new_contact(self, wd, contact):
-        # initiate adding contact
-        wd.find_element_by_link_text("add new").click()
-        # fill in some fields
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.last_name)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        # create contact
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def test_test_add_contact(self):
-        wd = self.wd
-        self.login(wd)
-        self.add_new_contact(wd, Contact("Mister", "Musterman", "+490123456789", "tester@test.com"))
-        self.logout(wd)
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
