df7782595669ff5e2e0f021835db91fac8c13abf
Rostyslav Gurevych <rostikg@gmail.com>
2016-12-16 10:28:37 +0100

    Homework 4 - decomposition (helper classes and manager class created)

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import pytest
-
 from fixture.application import Application
 from models.group import Group
 
@@ -14,11 +13,11 @@ def app(request):
 
 def test_add_normal_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_new_group(Group(name="test group 1", header="header 1", footer="footer 1"))
+    app.group.create(Group(name="test group 1", header="header 1", footer="footer 1"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_new_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
