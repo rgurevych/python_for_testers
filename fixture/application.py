1d9df1f8f1b371131b6b752f9ef36b15901586ff
Rostyslav Gurevych <rostikg@gmail.com>
2017-01-15 00:11:11 +0100

    Homework 16 (command line options added)

--- fixture/application.py
+++ fixture/application.py
@@ -1,21 +1,30 @@
+from selenium import webdriver
 # для запуска в браузере Chrome откомментить следующую строку:
-from selenium.webdriver.chrome.webdriver import WebDriver
+# from selenium.webdriver.chrome.webdriver import WebDriver
 # для запуска в браузере Firefox откомментить следующие две строки:
 # from selenium.webdriver.firefox.webdriver import WebDriver
-# from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver()     #откомментировать для запуска в Chrome
-        # self.wd = WebDriver(firefox_binary=FirefoxBinary("C:/Program Files/Firefox_ESR/firefox.exe"))
-        # self.wd.implicitly_wait(5)
+    def __init__(self, browser, base_url):
+        if browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "firefox":
+            self.wd = webdriver.Firefox(firefox_binary=FirefoxBinary("C:/Program Files/Firefox_ESR/firefox.exe"))
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        elif browser == "opera":
+            self.wd == webdriver.Opera()
+        else:
+            raise ValueError("Unknown browser: %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
 
     def is_valid(self):
@@ -28,7 +37,7 @@ class Application:
 
     def open_app_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
 
     def destroy(self):
--- tests/test_delete_contact.py
+++ tests/test_delete_contact.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from models.contact import Contact
 from random import randrange
+import time
 
 def test_delete_any_contact(app):
     if app.contact.count() == 0:
@@ -8,6 +9,7 @@ def test_delete_any_contact(app):
     old_contacts = app.contact.get_contacts_list()
     index = randrange(len(old_contacts))
     app.contact.delete_any_contact(index)
+    #time.sleep(5)
     assert len(old_contacts) == app.contact.count() + 1
     new_contacts = app.contact.get_contacts_list()
     old_contacts[index:index+1] = []
