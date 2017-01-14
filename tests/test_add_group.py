eb78d47878d74fbf7e410631184dd450dad4a251
Rostyslav Gurevych <rostikg@gmail.com>
2017-01-14 16:23:18 +0100

    Random data generator implemented for group adding test

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,23 +1,37 @@
 # -*- coding: utf-8 -*-
-
+import pytest
 from models.group import Group
+import random
+import string
+import re
 
 
-def test_add_normal_group(app):
+def random_string(prefix, max_length):
+    sym = string.ascii_letters + string.digits + " "*20
+    #The following string generates random string, replaces duplicated spaces with only one and deletes space in the end
+    return re.sub('\s+', ' ', (prefix + "".join([random.choice(sym)
+                                                 for i in range(random.randrange(max_length))]).rstrip()))
+
+
+testdata = [Group(name="", header="", footer="")] + [
+           Group(name=random_string("name", 10), header=random_string("head", 15), footer=random_string("foot", 15))
+           for i in range(5)
+           ]
+
+
+"""testdata = [Group(name=name, header=header, footer=footer)
+            for name in ["", random_string("gname", 10)]
+            for header in ["", random_string("ghead", 15)]
+            for footer in ["", random_string("gfoot", 12)]
+    ]"""
+
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_random_group(app, group):
     old_groups = app.group.get_group_list()
-    group_for_adding = Group(name="test group 1", header="header 1", footer="footer 1")
+    group_for_adding = group
     app.group.create(group_for_adding)
     assert len(old_groups) == app.group.count() - 1
     new_groups = app.group.get_group_list()
     old_groups.append(group_for_adding)
     assert sorted(old_groups, key=Group.group_id_or_max) == sorted(new_groups, key=Group.group_id_or_max)
-
-
-"""def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group_for_adding = Group(name="", header="", footer="")
-    app.group.create(group_for_adding)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups) - 1
-    old_groups.append(group_for_adding)
-    assert sorted(old_groups, key=Group.group_id_or_max) == sorted(new_groups, key=Group.group_id_or_max)"""
