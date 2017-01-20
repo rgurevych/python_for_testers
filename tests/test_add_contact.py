1c88eef1603f48446b15f551a049b7908b600e7c
Rostyslav Gurevych <rostikg@gmail.com>
2017-01-20 21:51:17 +0100

    Test data is separated and moved to separate files

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,35 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-import random
-import string
-import re
 from models.contact import Contact
-
-
-def random_string(max_length):
-    sym = string.ascii_letters + string.digits + " "*10
-    return re.sub('\s+', ' ', ("".join([random.choice(sym) for i in range(random.randrange(3, max_length))]).strip()))
-
-
-def random_email():
-    domen = [".com", ".org", ".ua", ".de", ".co.uk", ".gov", ".eu", ".tv"]
-    return re.sub('\s+', '', random_string(10)) + "@" + re.sub('\s+', '',random_string(10)) + random.choice(domen)
-
-
-def random_phone():
-    return "+" + "".join([random.choice(string.digits) for i in range(random.randrange(6, 12))])
-
-
-def random_address():
-    return random_string(20) + "\n" + random_string(20) + "\n" + random_string(20)
-
-
-testdata = [Contact(first_name="", last_name="", email_1="", mobile_phone="", address="")] + [
-           Contact(first_name=random_string(10), last_name=random_string(15),
-                   mobile_phone=random_phone(), work_phone=random_phone(), home_phone=random_phone(), fax=random_phone(),
-                   email_1=random_email(), email_2=random_email(), email_3=random_email(), address=random_address())
-           for i in range(5)
-           ]
+from data.add_contact import testdata
 
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,30 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 from models.group import Group
-import random
-import string
-import re
-
-
-def random_string(prefix, max_length):
-    sym = string.ascii_letters + string.digits + " "*20
-    #The following string generates random string, replaces duplicated spaces with only one and deletes space in the end
-    return re.sub('\s+', ' ', (prefix + "".join([random.choice(sym)
-                                                 for i in range(random.randrange(max_length))]).rstrip()))
-
-
-testdata = [Group(name="", header="", footer="")] + [
-           Group(name=random_string("name", 10), header=random_string("head", 15), footer=random_string("foot", 15))
-           for i in range(5)
-           ]
-
-
-"""testdata = [Group(name=name, header=header, footer=footer)
-            for name in ["", random_string("gname", 10)]
-            for header in ["", random_string("ghead", 15)]
-            for footer in ["", random_string("gfoot", 12)]
-    ]"""
-
+from data.add_group import testdata
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_random_group(app, group):
